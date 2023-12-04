0a329669e82b967481fcf8e8fdef7d5c788f66bc
TR <2993078435@qq.com>
2023-12-04 17:25:43 +0800

    deploy

---
+++ precache-manifest.cef8e5352f2a3aa64381cdc16a93ddf9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce5b6242bdc4bc1a330d",
+    "url": "/Manei/css/app.c662fe4b.css"
+  },
+  {
+    "revision": "9444d58a49b0ac4adef7a6c7715d4fde",
+    "url": "/Manei/index.html"
+  },
+  {
+    "revision": "ce5b6242bdc4bc1a330d",
+    "url": "/Manei/js/app.508bbfc1.js"
+  },
+  {
+    "revision": "3088bc01e4c9a03b57c3",
+    "url": "/Manei/js/chunk-vendors.11b051ea.js"
+  },
+  {
+    "revision": "1db034e61d7c91c93f36a32154f22731",
+    "url": "/Manei/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Manei/robots.txt"
+  }
+]);
