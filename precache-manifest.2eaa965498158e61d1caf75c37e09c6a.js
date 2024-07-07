7a0a04aa716803712cf50fc75eb3df667936066e
Ji7mmy <2993078435@qq.com>
2024-07-07 18:31:05 +0800

    deploy

---
+++ precache-manifest.2eaa965498158e61d1caf75c37e09c6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebff25830ea7e7b84e96",
+    "url": "/Manei/css/app.d35e99b1.css"
+  },
+  {
+    "revision": "ed1783459b1c62da295bdd2ca39c052b",
+    "url": "/Manei/index.html"
+  },
+  {
+    "revision": "ebff25830ea7e7b84e96",
+    "url": "/Manei/js/app.c9260b45.js"
+  },
+  {
+    "revision": "14b7b626e43146625c4c",
+    "url": "/Manei/js/chunk-vendors.2a6e232c.js"
+  },
+  {
+    "revision": "1db034e61d7c91c93f36a32154f22731",
+    "url": "/Manei/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Manei/robots.txt"
+  }
+]);
